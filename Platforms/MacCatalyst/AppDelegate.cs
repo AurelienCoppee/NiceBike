3c106dcc78e80007bf7d3e674309ae31cb24af75
Laizo <aureliencoppee@gmail.com>
2023-02-07 15:07:40 +0100

    Initialisation

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace NiceBike;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
