7a59bb1664da1f3f39efb1ae9a913f7a96fd1936
ChillinSoul <99248410+ChillinSoul@users.noreply.github.com>
2023-02-21 12:50:36 +0100

    add the possibility to display stocks from the db

--- CatalogPage.xaml.cs
+++ CatalogPage.xaml.cs
@@ -47,11 +47,21 @@ namespace NiceBike
         private void AddToCart(object sender, EventArgs e)
         {
             var button = sender as Button;
+            
             var bike = button?.BindingContext as Bike;
             if (bike != null)
             {
                 App.Cart.AddToCart(bike);
                 Application.Current.MainPage.Navigation.PushAsync(new CartPage());
+                if (bike.BuiltStock > 0)
+                {
+                    bike.BuiltStock--;
+
+                }
+                else if (bike.PartStock > 0)
+                {
+                    bike.PartStock--;
+                }
             }
         }
 
--- ViewModels/CatalogViewModel.cs
+++ ViewModels/CatalogViewModel.cs
@@ -24,9 +24,9 @@ namespace NiceBike
             // Initialize the Bikes collection with some sample data
             Bikes = new ObservableCollection<Bike>
             {
-                new Bike { Name = "City Bike", Description = "Simple bike for city travels with all needed parts like mudguards and lights", Price = 499.99M, SizesAndColors = "Available in sizes 26\" and 28\", in blue or red", Image = "test.jpg" },
-                new Bike { Name = "Explorer Bike", Description = "A mountain bike with wider tires and more grooved, and adapted mudguards", Price = 699.99M, SizesAndColors = "Available in sizes 26\" and 28\", in green or black", Image = "test.jpg" },
-                new Bike { Name = "Adventure Bike", Description = "A mountain bike with reinforced frame, no luggage rack, mudguards or light", Price = 799.99M, SizesAndColors = "Available in sizes 26\" and 28\", in yellow or orange", Image = "test.jpg" }
+                new Bike { Name = "City Bike", Description = "Simple bike for city travels with all needed parts like mudguards and lights", Price = 499.99M, SizesAndColors = "Available in sizes 26\" and 28\", in blue or red", Image = "bike.jpg" ,PartStock = GetStockParts(),BuiltStock = GetStockBuilt()},
+                new Bike { Name = "Explorer Bike", Description = "A mountain bike with wider tires and more grooved, and adapted mudguards", Price = 699.99M, SizesAndColors = "Available in sizes 26\" and 28\", in green or black", Image = "bike.jpg",PartStock = GetStockParts() ,BuiltStock = GetStockBuilt()},
+                new Bike { Name = "Adventure Bike", Description = "A mountain bike with reinforced frame, no luggage rack, mudguards or light", Price = 799.99M, SizesAndColors = "Available in sizes 26\" and 28\", in yellow or orange", Image = "bike.jpg",PartStock = GetStockParts() ,BuiltStock = GetStockBuilt()}
             };
 
             foreach (var bike in Bikes)
@@ -36,6 +36,32 @@ namespace NiceBike
                     await Application.Current.MainPage.Navigation.PushAsync(new BikeDetailPage(Bikes, bike.Id));
                 });
             }
+        }
+        private int GetStockBuilt()
+        {
+            //Query the db with the name of the part
+            return 5;
+        }
+        private int GetStockParts()
+        {
+            List<(string,int)> partList = new List<(string,int)>
+            {
+                ("frame",1),
+                ("tire",2),
+                ("handlebar",1),
+                ("screw",12)
+            };
+            int leastBuildable = 1000000;
+            foreach ((string, int) part in partList)
+            {
+                int avQuentity = 20;//Query the db with the name of the part
+                avQuentity = avQuentity / part.Item2;
+                if (avQuentity< leastBuildable)
+                {
+                    leastBuildable = avQuentity;
+                }
+            }
+            return leastBuildable;
         }
     }
 }
