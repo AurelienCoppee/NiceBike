b4d1a9f549599083468aedf07663e6df3601dca0
LEBaz2211 <92997786+LEBaz2211@users.noreply.github.com>
2023-02-21 12:45:44 +0100

    beginning decorator

---
+++ Models/AbstractBike.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NiceBike.Models;
+
+public abstract class AbstractBike
+{
+    public abstract int IdCounter { get; }
+    
+    public abstract string Description { get; }
+    public abstract double Price { get; }
+    public abstract int Id { get; set; }
+    public abstract string Name { get; set; }
+    public abstract string SizesAndColors { get; set; }
+    public abstract string Image { get; set; }
+}
