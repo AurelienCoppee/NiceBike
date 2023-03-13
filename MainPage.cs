c7c2bbc58239475531e489c2c8965ec7a8eb9936
KolciFranceska <195387@ecam.be>
2023-03-13 09:26:26 +0100

    Begin Shell page

---
+++ MainPage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Maui.Controls;
+using NiceBike;
+
+public class MainPage : Shell {
+    public MainPage() {
+        // Ajoutez vos pages ici
+        AddTab(new CatalogPage(), "Catalog");
+    }
+
+    private void AddTab(Page page, string title) {
+        Items.Add(new ShellContent { Content = page, Title = title });
+    }
+
+}
